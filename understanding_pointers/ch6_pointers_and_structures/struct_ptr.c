3ca02ededc35d64a1b05c3d43d5b6060dd043d6c
Bhaskar Thakur <bhaskar.dezire@gmail.com>
2025-02-03 14:17:32 +0530

    Fix memory allocation in struct_ptr.c [ch5]

--- understanding_pointers/ch6_pointers_and_structures/struct_ptr.c
+++ understanding_pointers/ch6_pointers_and_structures/struct_ptr.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct _person {
     char* name;
@@ -9,10 +10,16 @@ typedef struct _person {
 int main() {
     Person* prsnPtr;
     prsnPtr = (Person*) malloc(sizeof(Person));
-    prsnPtr->name = "Bhaskar";
+    // We need to allocate memory separately because name only stores the address of the first character and
+    // not the total value (i.e. "Bhaskar") inside of it
+    prsnPtr->name = (char*) malloc(strlen("Bhaskar")+1);
+    strcpy(prsnPtr->name, "Bhaskar");
     prsnPtr->age = 87;
 
     printf("Name: %s, Age: %d\n", prsnPtr->name, prsnPtr->age);
+
+    free(prsnPtr->name);
+    free(prsnPtr);
 
     return 0;
 }
