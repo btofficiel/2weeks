9dd6fe7940c07107c711f881a0cb7bc4db6fabc1
Bhaskar Thakur <bhaskar.dezire@gmail.com>
2025-04-11 15:49:23 +0530

    Add webserver.py

---
+++ computer_networking_tda/ch2_application_layer/python/sock_assign_webserver.py
@@ -0,0 +1,33 @@
+#import socket module
+import os
+from datetime import datetime, timezone
+from socket import *
+import sys # In order to terminate the program
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', 5000))
+serverSocket.listen()
+while True:
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        filepath = f'./files{filename}'
+        f = open(filepath)
+        last_modified = os.path.getmtime(filepath)
+        size_bytes = os.path.getsize(filepath)
+        utc_datetime = datetime.fromtimestamp(last_modified, timezone.utc)
+        formatted_date = utc_datetime.strftime("%a, %d %b %Y %H:%M:%S GMT")
+        contents = f.read()
+        outputdata = f'HTTP/1.1 200 OK\r\nDate: {formatted_date}\r\nContent-Length: {size_bytes}\r\nContent-Type: text/html; charset=UTF-8\r\nConnection: close\r\n\r\n{contents}\r\n\r\n'
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.close()
+    except IOError as err:
+        error_resp = f'HTTP/1.1 400 Not Found\r\n\r\n'
+        for i in range(0, len(error_resp)):
+            connectionSocket.send(error_resp[i].encode())
+        connectionSocket.close()
+serverSocket.close()
+sys.exit()#Terminate the program after sending the corresponding data
